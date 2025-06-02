f8afae0f1632d9413a00ac6a1968ea7fb7f98a5b
GuilhermeLino <joseguilhermelino3005@gmail.com>
2025-06-02 14:24:08 -0300

    Adicionando a interface Assento

--- src/estadio/Assento.java
+++ src/estadio/Assento.java
@@ -1,5 +1,11 @@
 package estadio;
 
 public interface Assento {
-
+    void inicializarAssentos();
+    void imprimirAssentos(int quantidadeDeSetores);
+    boolean reservarAssento(int fila, int assento, int quantidadeDeSetores);
+    boolean liberarAssento(int fila, int assento, int quantidadeDeSetores);
+    Setor getSetor();
 }
+
+
