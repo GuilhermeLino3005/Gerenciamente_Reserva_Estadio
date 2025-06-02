0b7851d8d9a52925a59c27e03e59fd635e6de747
GuilhermeLino <joseguilhermelino3005@gmail.com>
2025-06-02 14:56:53 -0300

    Criando a classe Local

--- src/estadio/Local.java
+++ src/estadio/Local.java
@@ -2,4 +2,29 @@ package estadio;
 
 public class Local {
 
+    private static Local instance;
+    private String nome;
+
+    protected Local(){
+
+    }
+
+    public static Local getInstance() {
+        if (instance == null) {
+            instance = new Local();
+        }
+        return instance;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public void Apresentacao() {
+        System.out.println("Esta é a apresentação genérica de um Local.");
+    }
 }
