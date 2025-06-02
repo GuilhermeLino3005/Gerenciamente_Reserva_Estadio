b5478e23344d1e4f1d33cba062bc0da4413649f6
GuilhermeLino <joseguilhermelino3005@gmail.com>
2025-06-02 14:47:11 -0300

    Criando o SetorFactory

--- src/estadio/SetorFactory.java
+++ src/estadio/SetorFactory.java
@@ -1,5 +1,19 @@
 package estadio;
 
 public class SetorFactory {
+    private static SetorFactory instance;
+
+    private SetorFactory() {}
+
+    public static SetorFactory getInstance() {
+        if (instance == null) {
+            instance = new SetorFactory();
+        }
+        return instance;
+    }
+
+    public Setor criarSetor(int id, String nome) {
+        return new SetorImpl(id, nome);
+    }
 
 }
